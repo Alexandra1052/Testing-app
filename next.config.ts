135a6910c8c02175a45a3eaca9f8dc909a09aa2d
Alexandra Ghita <anda.ghita05@gmail.com>
2025-02-19 00:23:00 +0200

    deploy commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  basePath: '/https://github.com/Alexandra1052/Testing-app.git', 
+  trailingSlash: true,
 };
 
 export default nextConfig;
