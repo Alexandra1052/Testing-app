5e6d247138d295206d7ac2ccb0fe0e8dd05bdf8e
Alexandra Ghita <anda.ghita05@gmail.com>
2025-02-19 00:36:58 +0200

    n

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  basePath: '/https://github.com/Alexandra1052/Testing-app.git', 
+  basePath: '/https://github.com/Alexandra1052/Testing-app.git',
   trailingSlash: true,
 };
 
